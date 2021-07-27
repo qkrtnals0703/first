f4c7a7ee616bd1d6e1775c4e55547e15e8af2707
qkrtnals0703 <qkrtnals7428@naver.com>
2021-07-27 22:34:38 +0900

    1일 1커밋 2일차

---
+++ qufk1/src/qufk1/plus.java
@@ -0,0 +1,18 @@
+package qufk1;
+
+import java.util.Scanner;
+
+public class plus {
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println(" ");
+	int a = scanner.nextInt();
+	System.out.println(" ");
+	int b = scanner.nextInt();	
+	
+	int sum = a + b;
+	
+	System.out.println(sum);
+	}
+}
