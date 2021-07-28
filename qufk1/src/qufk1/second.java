07b1991e2a9704fa0a55140609fc05a5956f5779
qkrtnals0703 <qkrtnals7428@naver.com>
2021-07-28 23:41:54 +0900

    1커밋 시러....

---
+++ qufk1/src/qufk1/second.java
@@ -0,0 +1,19 @@
+package qufk1;
+
+import java.util.Scanner;
+
+public class second {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(" ");
+		int a = scanner.nextInt();
+		System.out.println(" ");
+		int b = scanner.nextInt();	
+		
+		int sum = a * b;
+		
+		System.out.println(sum);
+		}
+
+}
