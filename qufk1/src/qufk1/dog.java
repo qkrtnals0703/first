73e8cdbff97e15c82cc325a3a35a8195a9849def
qkrtnals0703 <qkrtnals7428@naver.com>
2021-08-03 23:31:01 +0900

    백준 10171 10172

---
+++ qufk1/src/qufk1/dog.java
@@ -0,0 +1,11 @@
+package qufk1;
+
+public class dog {
+	public static void main(String[] args) {
+	System.out.println("|\\_/|");
+	System.out.println("|q p|   /}");
+	System.out.println("( 0 )\"\"\"\\");
+	System.out.println("|\"^\"`    |");
+	System.out.println("||_/=\\\\__|");
+	}
+}
