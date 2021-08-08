1896a23d1541a549c4f8cf11bdea8e55a244a971
qkrtnals0703 <qkrtnals7428@naver.com>
2021-08-08 18:32:12 +0900

    백준 2741

---
+++ qufk1/src/qufk1/n_2741.java
@@ -0,0 +1,15 @@
+package qufk1;
+
+import java.util.Scanner;
+
+public class n_2741 {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
