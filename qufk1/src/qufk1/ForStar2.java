6dfb7981d688e17cab073aed245da00e3aaab64b
qkrtnals0703 <qkrtnals7428@naver.com>
2021-08-13 22:57:14 +0900

    백준 2439

---
+++ qufk1/src/qufk1/ForStar2.java
@@ -0,0 +1,21 @@
+package qufk1;
+
+import java.util.Scanner;
+
+public class ForStar2 {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int h=0; h<i; h++) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
